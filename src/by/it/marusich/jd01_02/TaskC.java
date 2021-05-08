88985d1aeb033c0fe913ab05d1e66cbf7da97cc5
marusich <marusitchdmitry@yandex.by>
2021-05-08 14:51:16 +0300

    first commit

---
+++ src/by/it/marusich/jd01_02/TaskC.java
@@ -0,0 +1,40 @@
+package by.it.marusich.jd01_02;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Random rand = new Random();
+        int x = rand.nextInt();
+
+        step1(x);
+    }
+
+    private static void step1(int x) {
+        for (int i = -5; i < 5; i++) {
+            System.out.println(i + " ");
+            if (i % 5 == 0) ;
+            System.out.println();
+
+        }
+
+    }
+
+    int[][] step3(int[][] array) {
+        int max = Integer.MAX_VALUE;
+        for (int[] row : array) {
+            for (int e : row) {
+                if (e > max) {
+                    max = e;
+                }
+            }
+        }
+        return
+    }
+    /*boolean[]delCol = new boolean[array[0].length];
+    boolean[]delCol = new boolean[array.length];
+
+*/

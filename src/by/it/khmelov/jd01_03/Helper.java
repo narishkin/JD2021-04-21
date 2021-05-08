7dcd27b17302fd764ebcdf490b791f55aedb3cf6
Vadim <wadim.sidoryk@mail.ru>
2021-05-08 14:50:24 +0300

    first commit

---
+++ src/by/it/khmelov/jd01_03/Helper.java
@@ -0,0 +1,10 @@
+package by.it.khmelov.jd01_03;
+
+import java.util.Arrays;
+
+public class Helper {
+
+    static void sort(double[] arr) {
+        Arrays.sort(arr);
+    }
+}
---
+++ src/by/it/khmelov/jd01_03/InOut.java
@@ -0,0 +1,36 @@
+package by.it.khmelov.jd01_03;
+
+public class InOut {
+
+
+    static double[] getArray(String line) {
+        String lineWithoutSpaces = line.trim();
+        String[] valuesStringArray = lineWithoutSpaces.split(" ");
+        double[] values = new double[valuesStringArray.length];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = Double.parseDouble(valuesStringArray[i]);
+        }
+        return values;
+    }
+
+    public static void printArray(double[] array) {
+        for (double element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * @param array input data
+     * @param nameArray name
+     * @param columnCount cols
+     */
+    public static void printArray(double[] array, String nameArray, int columnCount) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%s[% -3d]=%-9.4f ", nameArray, i, array[i]);
+            if (i == array.length - 1 || (i + 1) % columnCount == 0) {
+                System.out.println();
+            }
+        }
+    }
+}

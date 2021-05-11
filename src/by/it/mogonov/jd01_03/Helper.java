4ae0f71d963d0460f7f20a8f5eb9fb0e506d2f0e
Mogonov <elm22244@gmail.com>
2021-05-12 00:53:51 +0300

    jd01_01

---
+++ src/by/it/mogonov/jd01_03/Helper.java
@@ -0,0 +1,36 @@
+package by.it.mogonov.jd01_03;
+
+import java.util.Arrays;
+
+public class Helper {
+
+    static double findMin(double[] arr) {
+        double min = arr[0];
+        for (double v : arr) {
+            if (v < min) {
+                min = v;
+            }
+        }
+
+        System.out.println(min);
+        return min;
+
+    }
+
+
+
+    static double findMax(double[] arr) {
+        Arrays.sort(arr);
+        double max = arr[arr.length - 1];
+        System.out.println(max);
+        return max;
+    }
+
+
+    static void sort(double[] arr) {
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}

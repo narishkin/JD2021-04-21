976811680bb6f9cdad792b842429d08f0b7b7fde
marusich <marusitchdmitry@yandex.by>
2021-05-12 21:38:43 +0300

    Исправления

---
+++ src/by/it/marusich/jd01_06/Poem.java
@@ -0,0 +1,4 @@
+package by.it.marusich.jd01_06;
+
+public class Poem {
+}
---
+++ src/by/it/marusich/jd01_06/TaskA1.java
@@ -0,0 +1,7 @@
+package by.it.marusich.jd01_06;
+
+public class TaskA1 {
+    public static void main(String[]args){
+       // String text = Poem.TEXT;
+    }
+}

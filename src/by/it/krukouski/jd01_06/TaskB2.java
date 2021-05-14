f277fe90b7eaa606e405781365be43a4a3d7ad04
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-05-15 00:08:56 +0300

    JD 01_06 TaskB2 part3

--- src/by/it/krukouski/jd01_06/TaskB2.java
+++ src/by/it/krukouski/jd01_06/TaskB2.java
@@ -1,50 +1,45 @@
 package by.it.krukouski.jd01_06;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB2 {
-    public static final String WORD_PATTERN = "[а-яёА-ЯЁ:;,\\s-]+(\\.)";
-    public static String[] sentences = new String[0];
+    public static final String WORD_PATTERN = "[а-яёА-ЯЁ:;,\\s-]+(\\.\\.\\.)?(\\.)";
 
     public static void main(String[] args) {
         StringBuilder text = new StringBuilder(Poem.TEXT);
-        Pattern p = Pattern.compile("\\s");
-        Matcher m = p.matcher(text);
-        Pattern p1 = Pattern.compile(WORD_PATTERN);
-        Matcher m1 = p1.matcher(text);
-        while (m.find()) {
-            String e = m.replaceAll(" ");
-            text.replace(0, text.length() - 1, e);
+        Pattern pattern = Pattern.compile(WORD_PATTERN);
+        Matcher matcher = pattern.matcher(text);
+        int countSentence = 0;
+        while (matcher.find()) {
+            countSentence++;
         }
-        //System.out.println(text);
-        while (m1.find()) {
-            String e = m1.group();
-            e = e.trim();
-            System.out.println(Arrays.toString(stringArray(e)));
+        System.out.println(countSentence);
+        matcher.reset();
+        String[] sentences = new String[countSentence];
+        int i = 0;
+        while (matcher.find()) {
+            sentences[i] = matcher.group().replace("\n", " ").trim();
+            i++;
+        }
+        for (int j = 0; j < sentences.length; j++) {
+            for (int k = 0; k < sentences.length - 1; k++) {
+                if (sentences[k].length() > sentences[k + 1].length()) {
+                    String temp = sentences[k];
+                    sentences[k] = sentences[k + 1];
+                    sentences[k + 1] = temp;
 
+                }
+
+            }
 
         }
 
-
+        for (String s : sentences) {
+            System.out.printf("%s\n", s);
+        }
 
     }
-
-    private static CharSequence[] stringArray(String e) {
-        String[] array = new String[1];
-        int length = 0;
-        for (int i = 0; i < array.length; i++) {
-            array[i]=e;
-            length = array[i].charAt(e.length()-1)-array[i].charAt(0);
-            System.out.println(length);
-
-        }
-
-        return array;
-    }
-
-
 }
 
 

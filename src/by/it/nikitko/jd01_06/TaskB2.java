5bf8e942b5d971f30fa82a1aba14d70e2ae2ebe1
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2021-05-14 20:19:38 +0300

    jd01._06 TaskB2 Done

--- src/by/it/nikitko/jd01_06/TaskB2.java
+++ src/by/it/nikitko/jd01_06/TaskB2.java
@@ -1,14 +1,17 @@
 package by.it.nikitko.jd01_06;
 
 import java.util.Arrays;
-import static jdk.nashorn.internal.objects.NativeString.trim;
+import java.util.Comparator;
+
+
 
 
 public class TaskB2 {
 
     public static void main(String[] args) {
 
-        String[] sentences = Poem.TEXT.split("[.!?]\\s*");
+      //  String[] sentences = Poem.TEXT.split("[^.][.!?]\\s*");
+        String[] sentences = Poem.TEXT.split("\\s*(?<!\\.)\\.(?!\\.)\\s*");
         formatText(sentences);
         sortSentences(sentences);
         printText(sentences);
@@ -16,14 +19,20 @@ public class TaskB2 {
 
     private static void formatText(String[] sentences) {
         for (int i = 0; i < sentences.length; i++) {
-            sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ\\s]", " ").replaceAll("\\s+", " ");
-            sentences[i] = trim(sentences[i]);
+            sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ\\s]", " ").replaceAll("\\s+", " ").trim();
         }
     }
 
     private static void sortSentences(String[] sentences) {
-        Arrays.sort(sentences, (x, y) -> x.split(" ").length - y.split(" ").length);
-         }
+        Arrays.sort(sentences,
+                new Comparator<String>() {
+                    @Override
+                    public int compare(String x, String y) {
+                        return x.length() - y.length();
+                    }
+                }
+        );
+    }
 
     private static void printText(String[] sentences) {
         for (String sentence : sentences) {
--- src/by/it/nikitko/jd01_06/TaskC1.java
+++ src/by/it/nikitko/jd01_06/TaskC1.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskC1 {
-    public static final String WORD_PATTERN = "[,а-яёА-ЯЁ-]+";
+    public static final String WORD_PATTERN = "[.,а-яёА-ЯЁ-]+";
 
 
     public static void main(String[] args) {

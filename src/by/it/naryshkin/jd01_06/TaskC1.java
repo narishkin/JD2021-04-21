7a01a1672c85dcb8e18d3e228ddbe87da5dd9454
KN <narishun@gmail.com>
2021-05-15 16:55:50 +0300

    jd01_06 TaskA,B,C

--- src/by/it/naryshkin/jd01_06/TaskC1.java
+++ src/by/it/naryshkin/jd01_06/TaskC1.java
@@ -10,20 +10,12 @@ public class TaskC1 {
         Matcher m1 = p1.matcher(Poem.text);
         String s2 = m1.replaceAll("\nJ");
 
-//        System.out.println(s2);
         String[] array;
         array = s2.split("J");
-//        Pattern removeN = Pattern.compile("\\n");
-//        String rN;
-//        for (int i = 0; i < array.length; i++) {
-//            rN = array[i].replace("\n", "");
-//            array[i] = rN;
-//        }
 
         int maxLength = 0;
         /*Определение максимальной длины строки в массиве*/
         for (int i = 0; i < array.length; i++) {
-//            System.out.print(array[i]);
             if (array[i].length() > maxLength) {
                 maxLength = array[i].length();
             }
@@ -34,12 +26,10 @@ public class TaskC1 {
         for (int i = 0; i < array.length; i++) {
             Matcher m2 = p2.matcher(array[i]);
             int diff = maxLength - array[i].length();
-//            System.out.println("Разница " + diff + " для строки " + i);
             int numberOfMatched = 0;
             while (m2.find()) {
                 numberOfMatched++;
             }
-//            System.out.println("NOM:" + numberOfMatched + " для строки " + i);
             m2 = p2.matcher(array[i]);
             int amountOfSpacesToAdd = diff / numberOfMatched;
             String temp = "";
@@ -54,20 +44,17 @@ public class TaskC1 {
             }
             array[i] = temp;
 
-            Pattern p3 = Pattern.compile("(?<=[А-Яа-яёЁ,.-:])( )");
+            Pattern p3 = Pattern.compile("(?<=[А-Я-а-яёЁ,.:])( )");
             Matcher m3 = p3.matcher(array[i]);
             int counter = 0;
             StringBuilder sb = new StringBuilder(array[i]);
             while (m3.find()) {
                 int start;
-//                System.out.println(m3.group());
                 spacesRemain = diff % numberOfMatched;
-//                System.out.println("оставшихся пробелов" + spacesRemain);
                 if (counter < spacesRemain && i<array.length-1) {
                     start = m3.start();
                     start = start + counter;
-//                    System.out.println(i);
-//                    System.out.println("start" + start);
+
                     sb.insert(start, " ");
 
                     counter++;
@@ -75,9 +62,10 @@ public class TaskC1 {
             }
             array[i] = sb.toString();
         }
-
+        StringBuilder sbFinal = new StringBuilder();
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
+            sbFinal.append(array[i]);
         }
+        System.out.println(sbFinal);
     }
 }

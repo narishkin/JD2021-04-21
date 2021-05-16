c67b0f1eedca681448e2ec869ed6f073aaa7f4c3
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-05-16 18:50:09 +0300

    JD 01_07 TaskA,TaskB

---
+++ src/by/it/krukouski/jd01_07/Runner.java
@@ -0,0 +1,27 @@
+package by.it.krukouski.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var val = new Scalar(3.145);
+        System.out.println(val);
+        Scalar scalarVar = new Scalar(3.145);
+        Var val1 = new Scalar(scalarVar);
+        System.out.println(val1);
+        Var val2 = new Scalar("3.145");
+        System.out.println(val2);
+        System.out.println();
+
+
+        double[] array = new double[]{1.0, 2.0, 4.0};
+        Var vector = new Vector(array);
+        System.out.println(vector);
+        Vector newVector1 = new Vector(array);
+        Var vector1 = new Vector((newVector1));
+        System.out.println(vector1);
+        Var vector2 = new Vector("{1.0, 2.0, 4.0}");
+        System.out.println(vector2);
+
+
+    }
+
+}

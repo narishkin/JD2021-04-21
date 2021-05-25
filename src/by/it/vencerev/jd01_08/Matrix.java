641ff9dd035a601c514aab246da799aa06013513
Alexisvencerev87 <vencerev87@gmail.com>
2021-05-25 18:33:25 +0300

    jd01_08 main taska1

---
+++ src/by/it/vencerev/jd01_08/Matrix.java
@@ -0,0 +1,13 @@
+package by.it.vencerev.jd01_08;
+
+import by.it._classwork_.jd01_08.Var;
+
+class Matrix extends Var {
+    public Matrix(String stringValues) {
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{}";
+    }
+}

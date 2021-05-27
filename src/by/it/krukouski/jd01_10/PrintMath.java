f18f56beaa51a7c980336054614005593c188496
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-05-27 23:27:56 +0300

    JD 01_10 Done

--- src/by/it/krukouski/jd01_10/PrintMath.java
+++ src/by/it/krukouski/jd01_10/PrintMath.java
@@ -1,7 +1,9 @@
 package by.it.krukouski.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.StringJoiner;
 
 public class PrintMath {
     public static void main(String[] args) {
@@ -10,22 +12,38 @@ public class PrintMath {
         Method[] declaredMethods = mathClass.getDeclaredMethods();
         for (Method method : declaredMethods) {
             int modifiers = method.getModifiers();
-            StringBuilder string = new StringBuilder();
+            StringBuilder stringMethod = new StringBuilder();
             if (Modifier.isPublic(modifiers)) {
-                string.append("public ");
+                stringMethod.append("public ");
+            } else continue;
+            if (Modifier.isStatic(modifiers)) {
+                stringMethod.append("static ");
             }
-            if (Modifier.isStatic(modifiers)){
-                string.append("static ");
+            stringMethod.append(method.getReturnType()).append(" ");
+            stringMethod.append(method.getName());
+
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            StringJoiner parameterString = new StringJoiner(",", "(", ")");
+            for (Class<?> parameterType : parameterTypes) {
+                parameterString.add(parameterType.toString());
             }
-            string.append(method.getReturnType()).append(" ");
-            string.append(method.getName());
-
-            System.out.println(string);
-
+            stringMethod.append(parameterString.toString());
+            System.out.println(stringMethod);
+        }
+        Field[] declaredFields = mathClass.getDeclaredFields();
+        for (Field field : declaredFields) {
+            int modifier = field.getModifiers();
+            StringBuilder stringField = new StringBuilder();
+            if (Modifier.isPublic(modifier)) {
+                stringField.append("public ");
+            } else continue;
+            if (Modifier.isStatic(modifier)) {
+                stringField.append("static ");
+            }
+            stringField.append(field.getType()).append(" ");
+            stringField.append(field.getName());
+            System.out.println(stringField);
 
         }
-
-
-
     }
 }

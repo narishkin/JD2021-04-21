47af292a45a5b1bc47130f48e10d9c21a204ba7e
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-06-01 15:27:45 +0300

    JD 01_11 TAskC part1

--- src/by/it/krukouski/jd01_11/Runner.java
+++ src/by/it/krukouski/jd01_11/Runner.java
@@ -1,27 +1,37 @@
 package by.it.krukouski.jd01_11;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Runner {
     public static void main(String[] args) {
         List<Integer> listA = new ListA<>();
         List<Integer> listB=new ListB<>();
         List<Integer> list2 = new ArrayList<>();
+        Set<Integer> setR=new HashSet<>();
+        Set<Integer> setC=new SetC<>();
+
 
         listB.add(1); list2.add(1);
         listB.add(2); list2.add(2);
         listB.add(3); list2.add(3);
         listB.add(4); list2.add(4);
-        System.out.println(listB);
-        System.out.println(list2);
+        //System.out.println(listB);
+        //System.out.println(list2);
 
-        //listB.add(2,8); list2.add(2,8);
+        listB.add(2,8); list2.add(2,8);
         listB.set(0,0); list2.set(0,0);
-        //listB.addAll(list2);
-        System.out.println(listB);
-        System.out.println(list2);
+        //System.out.println(listB);
+        //System.out.println(list2);
 
+        setC.add(5); setR.add(5);
+        setC.add(4); setR.add(4);
+        setC.add(3); setR.add(3);
+        setC.add(4); setR.add(4);
+        System.out.println(setC);
+        System.out.println(setR);
 
 
 
--- src/by/it/krukouski/jd01_11/SetC.java
+++ src/by/it/krukouski/jd01_11/SetC.java
@@ -1,13 +1,49 @@
 package by.it.krukouski.jd01_11;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
 public class SetC<T> implements Set<T> {
+
+    private T[] array = (T[]) new Object[]{};
+    private int size = 0;
+
+
+    @Override
+    public boolean add(T t) {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, (size * 3) / 2 + 1);
+
+            for (T elementArray : array) {
+                if (t.equals(elementArray)) {
+                    return false;
+                } else {
+                    array[size++] = t;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
     @Override
     public int size() {
-        return 0;
+        int resultSize = 0;
+        for (T elementArray : array) {
+            resultSize++;
+        }
+        return resultSize;
     }
 
     @Override
@@ -16,8 +52,30 @@ public class SetC<T> implements Set<T> {
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean addAll(Collection<? extends T> c) {
         return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        String delimiter = "";
+        for (int i = 0; i < size; i++) {
+            sb.append(delimiter).append(array[i]);
+            delimiter = ", ";
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     @Override
@@ -36,32 +94,7 @@ public class SetC<T> implements Set<T> {
     }
 
     @Override
-    public boolean add(T t) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
         return false;
     }
 

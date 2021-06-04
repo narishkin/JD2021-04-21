81aac51e29ea1ec304d49deaf5444d47d87840a8
Mogonov <elm22244@gmail.com>
2021-06-04 21:39:17 +0300

    jd01_11 A

--- src/by/it/mogonov/jd01_09/Parser.java
+++ src/by/it/mogonov/jd01_09/Parser.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 public class Parser {
     Var calc(String expression) {
         String[] operand = expression.split(Patterns.OPERATION);
-Var one=Var.createVAR(operand[0]);
+        Var one=Var.createVAR(operand[0]);
         Var two=Var.createVAR(operand[1]);
         if(one==null || two==null)
-            return null;
+            return null;//TODO create error
         Pattern p=Pattern.compile(Patterns.OPERATION);
         Matcher m=p.matcher(expression);
         if(m.find()) {

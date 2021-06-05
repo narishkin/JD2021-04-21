24cb58c66602c8bad01a51c60f05ddf9cff75251
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2021-06-05 16:50:03 +0300

    jd01._13 Task A

---
+++ src/by/it/nikitko/jd01_13/TaskA.java
@@ -0,0 +1,26 @@
+package by.it.nikitko.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        }
+
+        catch (NullPointerException|NumberFormatException e){
+            Class<TaskA> taskAClass = TaskA.class;
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTrace) {
+                if (stackTraceElement.getClassName().equals(taskAClass.getName())) {
+                    System.out.println("name:  "+e);
+                    System.out.println("Class: "+stackTraceElement.getClassName());
+                    System.out.println("Line   "+stackTraceElement.getLineNumber());
+                }
+            }
+        }
+    }
+}

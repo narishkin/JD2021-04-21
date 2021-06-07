ad3be879ee927a2d00e66cb5a33b885703e80a6e
loggoff <loggoff@tut.by>
2021-06-07 22:20:30 +0300

    Jd01_10 & jd01_11

---
+++ src/by/it/karpiuk/jd01_09/Patterns.java
@@ -0,0 +1,7 @@
+package by.it.karpiuk.jd01_09;
+
+public class Patterns {
+    static final String operation="[-+/*]";
+    static final String scalar="-?[0-9]+\\.?[0-9]*";
+    static final String vector="\\{((-?[0-9]+\\.?[0-9]*),?)+}";
+}

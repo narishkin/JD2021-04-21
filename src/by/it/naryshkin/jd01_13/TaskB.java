6667df405e8ca5b508fde5e90a4d9cea96567ba4
KN <narishun@gmail.com>
2021-06-07 16:49:01 +0300

    jd01_13 + jd01_12 C2C3

---
+++ src/by/it/naryshkin/jd01_13/TaskB.java
@@ -0,0 +1,39 @@
+package by.it.naryshkin.jd01_13;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class TaskB {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s;
+        double sum = 0;
+        double value;
+        double result;
+        try {
+            while (!(s = sc.next()).equals("END")) {
+                value = Double.parseDouble(s);
+                System.out.println(value);
+                sum = sum + value;
+                result = Math.sqrt(sum);
+                if (Double.isNaN(result)) {
+                    throw new ArithmeticException();
+                }
+                System.out.println(result);
+            }
+        } catch (NumberFormatException | ArithmeticException e) {
+            StackTraceElement[] STE = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : STE) {
+                if (TaskB.class.getName().equals(stackTraceElement.getClassName())) {
+                    String name = e.getClass().getName();
+                    String className = stackTraceElement.getClassName();
+                    int rowNumber = stackTraceElement.getLineNumber();
+                    System.out.printf("name: %s\nclass: %s\nline: %d", name, className, rowNumber);
+                    break;
+                }
+            }
+        }
+    }
+}

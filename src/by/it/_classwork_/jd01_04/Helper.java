1515e1c3c596d5e5c34dba12927f6aea14f9b9a5
Alexander Khmelov <Khmelov@users.noreply.github.com>
2021-06-09 16:35:25 +0300

    Update Helper.java

--- src/by/it/_classwork_/jd01_04/Helper.java
+++ src/by/it/_classwork_/jd01_04/Helper.java
@@ -2,9 +2,9 @@ package by.it._classwork_.jd01_04;
 
 import java.util.Arrays;
 
-class Helper {
+public class Helper {
 
-    public static void sort(double[] arr) {
+    static void sort(double[] arr) {
         Arrays.sort(arr);
     }
 }

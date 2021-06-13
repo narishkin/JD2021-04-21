cedbd8a3fe37c623ad8f3b517e14cf541ee0d180
Ivan Nikitko <Nikitko.ivan@gmail.com>
2021-06-13 12:52:20 +0300

    jd01_15 TaskB part

--- src/by/it/nikitko/jd01_15/TaskB.java
+++ src/by/it/nikitko/jd01_15/TaskB.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class TaskB {
     public static void main(String[] args) {
 
-
         String fileName = PathCreator.getPath(TaskB.class) + "TaskB.java";
         readToConsole(fileName);
     }
@@ -13,7 +12,7 @@ public class TaskB {
     private static void readToConsole(String fileName) {
         try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
             int line = fileReader.read();
-            while (line!=-1) {
+            while (line != -1) {
                 char a = (char) line;
                 System.out.println(a);
                 line = fileReader.read();
@@ -23,18 +22,5 @@ public class TaskB {
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
-
-
-
-    /*  private static void readToConsole(String fileName) {
-        try (BufferedInputStream fileReader = new BufferedInputStream(new FileInputStream(fileName))) {
-            int line = fileReader.read();
-            while (fileReader.available()>0) {
-                System.out.println(line);
-                line = fileReader.read();
-            }
-        } catch (IOException e) {
-            new RuntimeException(e);
-        }
-    }*/
-}}
+    }
+}

b0018a4fdd5549a40c5e1c91e45ee620ce09515f
Ivan Nikitko <Nikitko.ivan@gmail.com>
2021-06-13 09:56:56 +0300

    jd01_15 TaskA

--- src/by/it/nikitko/jd01_15/TaskA.java
+++ src/by/it/nikitko/jd01_15/TaskA.java
@@ -1,38 +1,33 @@
 package by.it.nikitko.jd01_15;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Random;
 
 public class TaskA {
 
-    private static Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
         int[][] matrix = new int[6][4];
         fillMatrix(matrix);
         String fileName= PathCreator.getPath(TaskA.class)+"matrix.txt";
         printMatrixTxt(matrix,fileName);
-        readToConsole(matrix,fileName);
+        readToConsole(fileName);
     }
 
-    private static void readToConsole(int[][] matrix, String fileName) {
-        try (FileReader fileReader = new FileReader(fileName)) {
-            for (int[] row : matrix) {
-                for (int element : row) {
-                    System.out.printf("%3d ",element);
-                }
-                System.out.println();;
+    private static void readToConsole( String fileName) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            String line = fileReader.readLine();
+            while (line!=null) {
+                System.out.println(line);
+                line = fileReader.readLine();
             }
 
         } catch (IOException e) {
-           new RuntimeException(e);
+            e.printStackTrace();
         }
     }
-
-    private static void printMatrixTxt(int[][] matrix, String fileName) {
+        private static void printMatrixTxt(int[][] matrix, String fileName) {
         try (PrintWriter printWriter = new PrintWriter(fileName)) {
             for (int[] row : matrix) {
                 for (int element : row) {
@@ -49,7 +44,7 @@ public class TaskA {
     private static void fillMatrix(int[][] matrix) {
         for (int[] row : matrix) {
             for (int i = 0; i < row.length; i++) {
-               row[i] = -15+random.nextInt(31);
+               row[i] = -15+ RANDOM.nextInt(31);
             }
         }
     }
---
+++ src/by/it/nikitko/jd01_15/TaskB.java
@@ -0,0 +1,39 @@
+package by.it.nikitko.jd01_15;
+
+import java.io.*;
+
+public class TaskB {
+    public static void main(String[] args) {
+
+
+        String fileName = PathCreator.getPath(TaskB.class) + "TaskB.java";
+        readToConsole(fileName);
+    }
+
+    private static void readToConsole(String fileName) {
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
+            int line = fileReader.read();
+            while (true) {
+                System.out.println(line);
+                line = fileReader.read();
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+
+
+    /*  private static void readToConsole(String fileName) {
+        try (BufferedInputStream fileReader = new BufferedInputStream(new FileInputStream(fileName))) {
+            int line = fileReader.read();
+            while (fileReader.available()>0) {
+                System.out.println(line);
+                line = fileReader.read();
+            }
+        } catch (IOException e) {
+            new RuntimeException(e);
+        }
+    }*/
+}}

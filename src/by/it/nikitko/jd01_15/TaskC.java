7b3a79e6d02099fcb4ff74c6fcd2edad4baf8575
Ivan <Nikitko.ivan@gmail.com>
2021-06-14 13:31:25 +0300

    jd01_15 TasksABC

--- src/by/it/nikitko/jd01_15/TaskC.java
+++ src/by/it/nikitko/jd01_15/TaskC.java
@@ -1,20 +1,13 @@
 package by.it.nikitko.jd01_15;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class TaskC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String path = PathCreator.getPath(TaskC.class);
-        System.out.println(path);
-       // Path path1 = Paths.get(path);
-
-       // Files currDir1 = new Files(path1);
-
+        System.out.print(path);
 
         while (true) {
             String inputString = scanner.nextLine();
@@ -23,20 +16,30 @@ public class TaskC {
             }
             if (inputString.equals("dir")) {
                 printDir(path);
-            } 
-            if (inputString.matches("[cd]")) {
-                changeDir(path);
+                System.out.print(path);
             }
-            
+            if (inputString.matches("cd\\s*\\.+")) {
+                File currDir = new File(path);
+                path = currDir.getParent();
+                System.out.print(path + File.separator);
+            }
+            if (!inputString.equals("dir") & !inputString.matches("cd\\s*\\.+")) {
+                path = changeDir(path, inputString);
+                System.out.print(path);
+            }
         }
     }
 
-    private static void changeDir(String path) {
+    private static String changeDir(String path, String dir) {
+        dir = dir.replaceAll("cd\\s*", "");
+        System.out.println(dir);
+        path = path + File.separator + dir + File.separator;
+        return path;
     }
 
     private static void printDir(String path) {
         File currDir = new File(path);
-        
+
         System.out.println();
         File[] filesCurrDir = currDir.listFiles();
         assert filesCurrDir != null;

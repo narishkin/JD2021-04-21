ea8932e48f02e1615c4dfe2696c2a55c8451d8cb
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-06-15 21:11:21 +0300

    JD 01_15 TaskC part1

--- src/by/it/krukouski/jd01_15/TaskC.java
+++ src/by/it/krukouski/jd01_15/TaskC.java
@@ -1,10 +1,28 @@
 package by.it.krukouski.jd01_15;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskC {
     public static void main(String[] args) {
+        String s = System.getProperty("user.dir") + File.separator
+                + "src" + File.separator
+                + "by" + File.separator
+                + "it" + File.separator
+                + "krukouski" + File.separator
+                + "jd01_15";
         Scanner sc = new Scanner(System.in);
-
+        File file = new File(s);
+        System.out.println(file);
+            String value = sc.next();
+            switch (value) {
+                case ("cd") :
+                    System.out.println(file.getParent());
+                case ("dir") :
+                    System.out.println(Arrays.toString(file.listFiles()));
+                case ("end") :
+                    break;
+            }
     }
 }

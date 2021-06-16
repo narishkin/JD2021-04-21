7285c6252634056d9dc7c23fa064f2252aa7cfc4
Vadim <wadim.sidoryk@mail.ru>
2021-06-16 21:30:06 +0300

    classwork jd02_01

--- src/by/it/_classwork_/jd02_01/Config.java
+++ src/by/it/_classwork_/jd02_01/Config.java
@@ -2,7 +2,7 @@ package by.it._classwork_.jd02_01;
 
 public class Config {
 
-    static final int K_SPEED = 10000;
+    static final int K_SPEED = 100;
 
     public static final int MAX_TIME = 120;
 

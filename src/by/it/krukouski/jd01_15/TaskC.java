7df8a3d0364edbb54a1d34597d05684b3bed8641
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-06-16 13:18:58 +0300

    JD 01_15 TaskC part2

--- src/by/it/krukouski/jd01_15/TaskC.java
+++ src/by/it/krukouski/jd01_15/TaskC.java
@@ -1,10 +1,16 @@
 package by.it.krukouski.jd01_15;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class TaskC {
+
+    static final String REGEX = "cd\\s\\.+";
+
     public static void main(String[] args) {
         String s = System.getProperty("user.dir") + File.separator
                 + "src" + File.separator
@@ -12,17 +18,72 @@ public class TaskC {
                 + "it" + File.separator
                 + "krukouski" + File.separator
                 + "jd01_15";
+        System.out.println(s);
         Scanner sc = new Scanner(System.in);
-        File file = new File(s);
-        System.out.println(file);
+        for (; ; ) {
             String value = sc.next();
-            switch (value) {
-                case ("cd") :
-                    System.out.println(file.getParent());
-                case ("dir") :
-                    System.out.println(Arrays.toString(file.listFiles()));
-                case ("end") :
-                    break;
+
+            if (value.equals("cd")) {
+                if (value.matches(REGEX)) {
+                    value = value.replaceAll("cd\\s*", "");
+                    s = s + File.separator + value + File.separator;
+                    System.out.println(s);
+                } else {
+                    File file = new File(s);
+                    s = file.getParent();
+                    System.out.println(s + File.separator);
+                }
             }
+            /*if (value.matches(REGEX)) {
+                File file = new File(s);
+                value = value.replaceAll("cd\\s*", "");
+
+                s = s + File.separator + value + File.separator;
+                System.out.println(s);
+            }
+
+             */
+            if (value.equals("dir")) {
+                getDir(s);
+            }
+            if (value.equals("end")) {
+                break;
+            }
+
+        }
+    }
+
+    private static void getDir(String s) {
+        File file = new File(s);
+        File[] arrayFiles = file.listFiles();
+        for (File elementArrayFiles : arrayFiles) {
+            if (elementArrayFiles.isFile()) {
+                Path path = Paths.get(s + File.separator + elementArrayFiles.getName());
+                try {
+                    System.out.printf("%20s         %10s %10s\n",
+                            Files.getAttribute(path, "lastModifiedTime"),
+                            Files.getAttribute(path, "size"),
+                            elementArrayFiles.getName()
+                    );
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (elementArrayFiles.isDirectory()) {
+                Path path = Paths.get(s + File.separator + elementArrayFiles.getName());
+                try {
+                    System.out.printf("%20s  <DIR>  %10s\n",
+                            Files.getAttribute(path, "lastModifiedTime"),
+                            elementArrayFiles.getName()
+                    );
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
 }

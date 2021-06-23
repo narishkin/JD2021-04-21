af8455f3fe4f309eb535076b83daa8905e9b528a
MikhailSapazhkou <m.sapazhkou@gmail.com>
2021-06-23 17:03:35 +0300

    jd02_01 (B-) Sapazhkou

--- src/by/it/sapazhkou/jd02_01/Basket.java
+++ src/by/it/sapazhkou/jd02_01/Basket.java
@@ -1,21 +1,40 @@
 package by.it.sapazhkou.jd02_01;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Basket {
 
     int idBuyer;
     ArrayList<Good> listGoods = new ArrayList<>();
 
-    Basket(int idBuyer){
+    public Basket(int idBuyer) {
         this.idBuyer = idBuyer;
     }
 
-    private void addToBasket(Good good){
+    public void addToBasket(Good good) {
         listGoods.add(good);
     }
 
-    private ArrayList getBasket(){
+    public ArrayList getBasket() {
         return listGoods;
     }
+
+//    public void toConsoleBasket(){
+//        HashMap good = new PriceOfGood().getGoods();
+//        double sum = 0.0;
+//        System.out.println("Bayer #"+idBuyer+" chose a goods:");
+//        for (int i = 0; i < listGoods.size(); i++) {
+//            System.out.print("good "+ listGoods.get(i).name()+" ");
+
+//            System.out.println("price "+ good.get(Good.valueOf(listGoods.get(i).name())));
+//            sum = sum + (double)good.get(Good.valueOf(listGoods.get(i).name()));
+//
+//        }
+////            System.out.printf("good %10s price %s",listGood.name(),listGood.toString());
+//
+//        System.out.println("All "+sum);
+//    }
+
+
 }
--- src/by/it/sapazhkou/jd02_01/Runner.java
+++ src/by/it/sapazhkou/jd02_01/Runner.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 public class Runner {
     public static void main(String[] args) {
 
-
         HashMap good = new PriceOfGood().getGoods();
         Object bread = (Double)good.get(Good.values()[2].toString());
+        Good value = Good.values()[0];
+        value.name();
 
-        System.out.println(bread.toString());
+        System.out.println("продукт "+Good.values()[2].name() +" цена "  + bread.toString());
 
         System.out.println(good.toString());
-
     }
 }

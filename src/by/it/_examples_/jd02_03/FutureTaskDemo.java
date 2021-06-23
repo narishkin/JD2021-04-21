25b459bd64f75a809ddc5d00a0c8d615fd6cd9da
Alexander Khmelov <akhmelev@gmail.com>
2021-06-23 16:41:58 +0300

    jd02_02

---
+++ src/by/it/_examples_/jd02_03/FutureTaskDemo.java
@@ -0,0 +1,29 @@
+package by.it._examples_.jd02_03;
+
+import java.util.concurrent.*;
+
+import static java.lang.Thread.sleep;
+
+public class FutureTaskDemo {
+
+    @SuppressWarnings("Convert2Lambda")
+    public static void main(String[] args) {
+        //в Java 8+ можно использовать лямбды
+        //Callable<Integer> callTh = () -> { sleep(500); return 500; };
+        Callable<Integer> callTh = new Callable<>() {
+            @Override
+            public Integer call() throws Exception {
+                sleep(500);
+                return 500;
+            }
+        };
+
+        FutureTask<Integer> task = new FutureTask<>(callTh);
+        task.run();
+        try {
+            System.out.print("Result:" + task.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}

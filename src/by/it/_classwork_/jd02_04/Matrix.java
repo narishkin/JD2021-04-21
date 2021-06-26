25c4076f75d63405533a413f8e5d70bea5910e90
Vadim <wadim.sidoryk@mail.ru>
2021-06-26 15:33:32 +0300

    classwork jd02_04

---
+++ src/by/it/_classwork_/jd02_04/Matrix.java
@@ -0,0 +1,11 @@
+package by.it._classwork_.jd02_04;
+
+class Matrix extends Var {
+    public Matrix(String stringValues) {
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix{}";
+    }
+}

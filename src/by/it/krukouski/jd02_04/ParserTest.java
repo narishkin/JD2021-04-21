d108feaada53d3f4ad5e63dc68ab5e493f1b0f18
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-06-29 00:01:17 +0300

    JD02_04 TaskA done

--- src/by/it/krukouski/jd02_04/ParserTest.java
+++ src/by/it/krukouski/jd02_04/ParserTest.java
@@ -12,17 +12,49 @@ public class ParserTest {
 
     @Before
     public void setUp() {
-        parser=new Parser();
+        parser = new Parser();
         System.out.println("Start");
     }
 
     @Test
-    public void calc() throws CalcException {
+    public void calc_1() throws CalcException {
         String expression = "A=2+5.3";
         Var actualVar = parser.calc(expression);
         double expected = 7.3;
         double actual = Double.parseDouble(actualVar.toString());
+        assertEquals(expected, actual, 1e-10);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_2() throws CalcException {
+        String expression = "B=A*3.5";
+        Var actualVar = parser.calc(expression);
+        double expected = 25.55;
+        double actual = Double.parseDouble(actualVar.toString());
+        assertEquals(expected, actual, 1e-10);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_3() throws CalcException {
+        String expression = "B1=B+0.11*-5";
+        Var actualVar = parser.calc(expression);
+        double expected = 25;
+        double actual = Double.parseDouble(actualVar.toString());
+        assertEquals(expected, actual, 1e-10);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+
+    @Test
+    public void calc_4() throws CalcException {
+        String expression = "B2=A/2-1";
+        Var actualVar = parser.calc(expression);
+        double expected = 2.65;
+        double actual = Double.parseDouble(actualVar.toString());
         assertEquals(expected,actual,1e-10);
+        System.out.printf("Expected: %s\nActual: %s\n", expected,actual);
     }
 
     @After

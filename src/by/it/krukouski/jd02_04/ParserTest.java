49d01b121a4d5b6b5ee2df580b6d0c9def074a9a
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-06-29 14:55:01 +0300

    JD 02_04 TaskB part1

--- src/by/it/krukouski/jd02_04/ParserTest.java
+++ src/by/it/krukouski/jd02_04/ParserTest.java
@@ -17,7 +17,7 @@ public class ParserTest {
     }
 
     @Test
-    public void calc_sum() throws CalcException {
+    public void calc_add_scalars() throws CalcException {
         String expression = "2+5.3";
         Var actualVar = parser.calc(expression);
         double expected = 7.3;
@@ -27,7 +27,7 @@ public class ParserTest {
     }
 
     @Test
-    public void calc_sub() throws CalcException {
+    public void calc_sub_scalars() throws CalcException {
         String expression = "4-3.5";
         Var actualVar = parser.calc(expression);
         double expected = 0.5;
@@ -37,7 +37,7 @@ public class ParserTest {
     }
 
     @Test
-    public void calc_mul() throws CalcException {
+    public void calc_mul_scalars() throws CalcException {
         String expression = "2*3";
         Var actualVar = parser.calc(expression);
         double expected = 6;
@@ -48,7 +48,7 @@ public class ParserTest {
 
 
     @Test
-    public void calc_div() throws CalcException {
+    public void calc_div_scalars() throws CalcException {
         String expression = "8/2";
         Var actualVar = parser.calc(expression);
         double expected = 4;
@@ -56,6 +56,87 @@ public class ParserTest {
         assertEquals(expected,actual,1e-10);
         System.out.printf("Expected: %s\nActual: %s\n", expected,actual);
     }
+
+    @Test
+    public void calc_add_vector_and_scalar() throws CalcException {
+        String expression = "{1,2,3}+2";
+        Var actualVar = parser.calc(expression);
+        String expected = "{3.0, 4.0, 5.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_add_vector_and_vector() throws CalcException {
+        String expression = "{1,2,3}+{1,2,3}";
+        Var actualVar = parser.calc(expression);
+        String expected = "{2.0, 4.0, 6.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_sub_vector_and_scalar() throws CalcException {
+        String expression = "{4,3,2}-1";
+        Var actualVar = parser.calc(expression);
+        String expected = "{3.0, 2.0, 1.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_sub_vector_and_vector() throws CalcException {
+        String expression = "{2,3,4}-{1,2,3}";
+        Var actualVar = parser.calc(expression);
+        String expected = "{1.0, 1.0, 1.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_mul_vector_and_scalar() throws CalcException {
+        String expression = "{4,3,2}*2";
+        Var actualVar = parser.calc(expression);
+        String expected = "{8.0, 6.0, 4.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_mul_vector_and_vector() throws CalcException {
+        String expression = "{2,3,4}*{1,2,3}";
+        Var actualVar = parser.calc(expression);
+        double expected = 20.0;
+        double actual = Double.parseDouble(actualVar.toString());
+        assertEquals(expected,actual, 1e-10);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
+    @Test
+    public void calc_div_vector_and_scalar() throws CalcException {
+        String expression = "{6,4,2}/2";
+        Var actualVar = parser.calc(expression);
+        String expected = "{3.0, 2.0, 1.0}";
+        String actual = actualVar.toString();
+        assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+    @Test
+    public void calc_div_vector_and_vector() throws CalcException {
+        String expression = "{4,3,2}/{4,3,2}";
+        Var actualVar = parser.calc(expression);
+        String expected = "ERROR: Division impossible";
+        String actual = actualVar.toString();
+        fail();
+        //assertEquals(expected,actual);
+        System.out.printf("Expected: %s\nActual: %s\n", expected, actual);
+    }
+
 
     @After
     public void tearDown() {

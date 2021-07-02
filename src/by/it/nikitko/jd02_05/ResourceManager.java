59076ae258a262ac370fd35138b0237619523b19
Ivan <Nikitko.ivan@gmail.com>
2021-07-02 19:34:34 +0300

    jd02_05 TaskAB

--- src/by/it/nikitko/jd02_05/ResourceManager.java
+++ src/by/it/nikitko/jd02_05/ResourceManager.java
@@ -1,5 +1,10 @@
 package by.it.nikitko.jd02_05;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -7,6 +12,8 @@ public enum ResourceManager {
     INSTANCE;
 
     private ResourceBundle resourceBundle;
+    private DateFormat dateFormat;
+    private DateTimeFormatter dateTimeFormatter;
 
     ResourceManager() {
         setLocale(Locale.getDefault());
@@ -14,9 +21,29 @@ public enum ResourceManager {
 
     public void setLocale(Locale locale) {
         resourceBundle = ResourceBundle.getBundle(PathCreator.getPath(ResourceManager.class), locale);
+     // dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
+         dateTimeFormatter = new DateTimeFormatter(locale);
     }
 
     public String get(String key) {
         return resourceBundle.getString(key);
+    }
+
+    public  void getDate() {
+
+
+     //   String s = dateFormat.format(new Date());
+       LocalDate today = LocalDate.now();
+
+
+        // String todayString = today.toString();
+       //    String s = dateFormat.format(todayString);
+        try {
+           Date d=null;
+                  d=  dateFormat.parse("April 12, 2016");
+            System.out.println(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/by/it/nikitko/jd02_05/Runner.java
+++ src/by/it/nikitko/jd02_05/Runner.java
@@ -8,11 +8,18 @@ public class Runner {
         if (args.length == 2) {
             Locale locale = new Locale(args[0], args[1]);
             manager.setLocale(locale);
+        } else {
+            Locale defaultLocale = new Locale("en", "US");
+            manager.setLocale(defaultLocale);
         }
         System.out.println(manager.get(Messages.WELCOME));
         System.out.println(manager.get(Messages.QUESTION));
         System.out.println(manager.get(User.FIRST_NAME));
         System.out.println(manager.get(User.LAST_NAME));
+
+
+        manager.getDate();
+
 
     }
 }

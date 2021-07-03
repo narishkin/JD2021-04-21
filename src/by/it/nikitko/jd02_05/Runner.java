aa8100f8c2a1e94c985457dfb002f7c1069e2280
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2021-07-03 10:46:04 +0300

    jd02_05 Tasks AB

--- src/by/it/nikitko/jd02_05/Runner.java
+++ src/by/it/nikitko/jd02_05/Runner.java
@@ -1,22 +1,50 @@
 package by.it.nikitko.jd02_05;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String enteredString;
         TimeUtils timeUtils = new TimeUtils();
         ResourceManager manager = ResourceManager.INSTANCE;
-        Locale locale= new Locale("en", "US") ;
+        Locale locale = new Locale("en", "US");
         manager.setLocale(locale);
+
         if (args.length == 2) {
-             locale = new Locale(args[0], args[1]);
+            locale = new Locale(args[0], args[1]);
             manager.setLocale(locale);
         }
+
+        printMessages(timeUtils, manager, locale);
+
+        while (!(enteredString = scanner.nextLine()).equals("end")) {
+            switch (enteredString) {
+                case ("ru"):
+                    locale = new Locale("ru", "RU");
+                    manager.setLocale(locale);
+                    break;
+
+                case ("en"):
+                    locale = new Locale("en", "US");
+                    manager.setLocale(locale);
+                    break;
+
+                case ("be"):
+                    locale = new Locale("be", "BY");
+                    manager.setLocale(locale);
+                    break;
+            }
+            printMessages(timeUtils, manager, locale);
+        }
+    }
+
+    private static void printMessages(TimeUtils timeUtils, ResourceManager manager, Locale locale) {
         System.out.println(manager.get(Messages.WELCOME));
         System.out.println(manager.get(Messages.QUESTION));
         System.out.println(manager.get(User.FIRST_NAME));
         System.out.println(manager.get(User.LAST_NAME));
         System.out.println(timeUtils.getDate(locale));
-
     }
 }
